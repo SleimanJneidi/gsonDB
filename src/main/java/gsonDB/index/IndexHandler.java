14527dce921c111ae5aceb838717421cb6e22fdb
Sleiman Jneidi <jneidi.sleiman@gmail.com>
2014-09-29 09:49:01 +0100

    add files' pointers

--- src/main/java/gsonDB/index/IndexHandler.java
+++ src/main/java/gsonDB/index/IndexHandler.java
@@ -4,6 +4,7 @@ import gsonDB.DB;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.Collections;
 import java.util.Map;
@@ -21,9 +22,16 @@ class IndexHandler {
     private final Class<?> entityType;
     private final File file;
     private final RandomAccessFile indexFile;
-    private final AtomicInteger numberOfRecords = new AtomicInteger(-1);
+
     private static final int NUM_OF_RECORDS_FILE_POINTER = 0; // file's head
+
     private static final int KEY_TABLE_FILE_POINTER = 4; // 4 bytes after the number of records
+    private static final int KEY_SIZE = 32; // 32 bytes
+    private static final int FILE_POINTER_SIZE = 8; // long value
+    private static final int RECORD_LENGTH_SIZE = 4; // integer value
+    private static final int INDEX_KEY_ENTRY_SIZE = KEY_SIZE + FILE_POINTER_SIZE + RECORD_LENGTH_SIZE;
+
+
 
     public static IndexHandler getIndexHandler(final Class<?> type,final DB db) throws FileNotFoundException {
         if (indexHandlers.containsKey(type)) {
@@ -46,6 +54,11 @@ class IndexHandler {
 
 
 
+    private int fetchNumberOfRecords() throws IOException {
+        this.indexFile.seek(NUM_OF_RECORDS_FILE_POINTER);
+        int numberOfRecords = this.indexFile.readInt();
+        return numberOfRecords;
+    }
 
     public static Map<Class<?>, IndexHandler> allIndexHandlers(){
         return Collections.unmodifiableMap(indexHandlers);
@@ -59,11 +72,11 @@ class IndexHandler {
         return entityType;
     }
 
-    public File getFile() {
+    protected File getFile() {
         return file;
     }
 
-    public RandomAccessFile getIndexFile() {
+    protected RandomAccessFile getIndexFile() {
         return indexFile;
     }
 }
